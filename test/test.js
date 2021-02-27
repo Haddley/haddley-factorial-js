ee2cae6642ed904918d007e92f3bd51b85f2afd6
Neil Haddley <neil@haddley.com>
2021-02-28 07:58:04 +1300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -39,5 +39,44 @@ describe('#factorial()', function () {
         expect(module.factorial_it(170)).to.equal(7.257415615308004e+306);
     })
 
+    // test tail recursion version
+    it('should calculate factorial of 171 using recursion', function () {
+        // add an assertion
+        expect(module.factorial(171)).to.equal(Infinity);
+    })
+
+    // test tail recursion version
+    it('should calculate factorial of 171 using tail recursion', function () {
+        // add an assertion
+        expect(module.factorial_tr(171)).to.equal(Infinity);
+    })
+
+    // test iteration version
+    it('should calculate factorial of 171 using iteration', function () {
+        // add an assertion
+        expect(module.factorial_it(171)).to.equal(Infinity);
+    })
+
+    // test recursive function
+    it('should calculate factorial of 10000 using recursion', function () {
+        this.timeout(10000);
+        // add an assertion
+        expect(module.factorial(10000)).to.equal(Infinity);
+    })
+
+    // test recursive function
+    it('should calculate factorial of 10000 using tail recursion', function () {
+        this.timeout(10000);
+        // add an assertion
+        expect(module.factorial_tr(10000)).to.equal(Infinity);
+    })
+
+    // test recursive function
+    it('should calculate factorial of 1600000 using tail recursion', function () {
+        this.timeout(10000);
+        // add an assertion
+        expect(module.factorial_it(1600000)).to.equal(Infinity);
+    })
+
 
 })
